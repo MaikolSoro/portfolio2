67e39470bc980900fb90035c0a1735b855f30718
Maikol <maikolsoro.z1998@gmail.com>
2023-07-08 19:19:34 -0700

    Updates

---
+++ precache-manifest.956ea85f48a9f8285c1c833e0142bcf7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08ce298aaf622012cc47906bb1960c71",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "87194d6ed6c48d7b4777",
+    "url": "/portfolio/static/js/2.7dabbbfc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.7dabbbfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd0b9c99973bfea832d",
+    "url": "/portfolio/static/js/main.557f6223.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "b0395048498c5b3c15aad495442a05cb",
+    "url": "/portfolio/static/media/avatar.b0395048.jpg"
+  },
+  {
+    "revision": "23199f871da2d0de4a454ab1e901dd59",
+    "url": "/portfolio/static/media/book.23199f87.png"
+  },
+  {
+    "revision": "e9da2a6d3b9185368c7a18b27aaa74b9",
+    "url": "/portfolio/static/media/chat.e9da2a6d.png"
+  },
+  {
+    "revision": "c6509f739c0f51dc5ccc78dcc4b7b6b2",
+    "url": "/portfolio/static/media/food.c6509f73.png"
+  },
+  {
+    "revision": "276876142cde46c1da81c4dbdf3b9202",
+    "url": "/portfolio/static/media/imc.27687614.png"
+  },
+  {
+    "revision": "cc85f49453ca5242e58cf4c907512d6a",
+    "url": "/portfolio/static/media/note.cc85f494.png"
+  },
+  {
+    "revision": "d9397228c923d1594471c8bcf98895b7",
+    "url": "/portfolio/static/media/product.d9397228.png"
+  },
+  {
+    "revision": "5b87fc1fe033984b1b9eabd9ceb93552",
+    "url": "/portfolio/static/media/task.5b87fc1f.png"
+  },
+  {
+    "revision": "306ebbfda116fe86b657b933669b05ea",
+    "url": "/portfolio/static/media/tracker.306ebbfd.png"
+  },
+  {
+    "revision": "1cfc964fd119c8929270d3d8b296b5a4",
+    "url": "/portfolio/static/media/utn.1cfc964f.svg"
+  }
+]);
